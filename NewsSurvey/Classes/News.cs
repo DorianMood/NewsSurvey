6ee976f753367991830c77ad66d6a4d4c681b56d
nekithockey75 <nikita01051997@gmail.com>
2018-05-13 15:01:10 +0500

    Add classes

---
+++ NewsSurvey/Classes/News.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NewsSurvey.Classes
+{
+    class News
+    {
+        // News properties
+        public string Number;
+        public string Publisher;
+        public string Url;
+        public string Header;
+        public string Text;
+
+        // Survey results
+        private Boolean ReliableSource { get; set; }
+        private Boolean Emotional { get; set; }
+        private int Reliability { get; set; }
+
+        public News(string __number, string __publisher, string __url, string __header, string __text)
+        {
+            Publisher = __publisher;
+            Url = __url;
+            Header = __header;
+            Text = __text;
+        }
+
+        public override string ToString()
+        {
+            return Number + ", " + Publisher + ", " + Url + ", " + Header + ", " + Text + "\n";
+        }
+    }
+}
--- NewsSurvey/MainWindow.xaml.cs
+++ NewsSurvey/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NewsSurvey.Classes;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace NewsSurvey
 {
@@ -20,9 +14,61 @@ namespace NewsSurvey
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<News> newsList;
+        const string FILE_LOCATION = "news.json";
+        int current = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            using (StreamReader reader = new StreamReader(FILE_LOCATION))
+            {
+                string json = reader.ReadToEnd();
+                newsList = JsonConvert.DeserializeObject<List<News>>(json);
+            }
+            SetElement(current);
+        }
+
+        private void SetElement(int index)
+        {
+            textCurrent.Text = index.ToString();
+            SetText(newsList[index].Text);
+        }
+
+        private void SetText(string text)
+        {
+            textNews.Document.Blocks.Clear();
+            textNews.Document.Blocks.Add(new Paragraph(new Run(text)));
+        }
+
+        private void buttonNext_Click(object sender, RoutedEventArgs e)
+        {
+            current = current < newsList.Count - 1 ? current + 1 : current;
+            SetElement(current);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            current = current > 0 ? current - 1 : current;
+            SetElement(current);
+
+        }
+
+        private void textCurrent_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                current = Convert.ToInt32(textCurrent.Text) >= 0 && Convert.ToInt32(textCurrent.Text) < newsList.Count ? Convert.ToInt32(textCurrent.Text) : current;
+                SetElement(current);
+                textCurrent.Text = current.ToString();
+            }
+        }
+        private void textCurrent_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textCurrent.SelectAll();
+            textCurrent.Focus();
         }
     }
 }
